d9c1490df2f9448b05e68cc3ec33e6b213c77926
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-10 17:30:07 +0700

    Menunjuk ke sebuah nama fille

---
+++ Iofile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    string baris ;
+    //Membuat file dalam mode menulis.
+    ofstream outfiles;
+    //Menunjuk ke sebuah nama file
+}
