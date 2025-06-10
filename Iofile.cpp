6799a443e668d3ec99e7f2ebe115fbb2dae20be4
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-10 17:45:57 +0700

    Membuat file

--- Iofile.cpp
+++ Iofile.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main () {
     string baris ;
     //Membuat file dalam mode menulis.
-    ofstream outfiles;
+    ofstream outfile;
     //Menunjuk ke sebuah nama file
 
-    outfile.open
+    outfile.open ("contohfile.txt");
+    cout << "Menulis file,\'q\' untuk keluar" << endl;
     while (true) {
         cout << "- ";
         //Mendapatkan setiap karakter dalam satu baris
