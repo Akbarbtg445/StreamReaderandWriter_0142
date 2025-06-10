570d65f0749d2e3fc5f8d1ce49a4bda04863054e
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-10 18:09:35 +0700

    Membuka file

---
+++ iofileWithpath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string Namafiles;
+    cout << "masukan Nama Files :";
+    cin >> Namafiles;
+    //Membuka file dalam mode menulis
+    ofstream outfiles;
+}
