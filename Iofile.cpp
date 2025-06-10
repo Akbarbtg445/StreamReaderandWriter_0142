58de70be7e6d90132688388d27527137b718e370
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-10 17:40:03 +0700

    Membuat loop

--- Iofile.cpp
+++ Iofile.cpp
@@ -8,4 +8,36 @@ int main () {
     //Membuat file dalam mode menulis.
     ofstream outfiles;
     //Menunjuk ke sebuah nama file
+
+    outfile.open
+    while (true) {
+        cout << "- ";
+        //Mendapatkan setiap karakter dalam satu baris
+        getline (cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris =="q") break;
+        //menulis dan memasukan nilai dari "baris" ke dalam file
+        outfile << baris << endl; 
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    ifstream infile;
+    
+    infile.open("contohfile.txt");
+    cout << endl << ">= membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline (infile,baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak di temukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
